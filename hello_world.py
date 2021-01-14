92ef6f83dd3e7f8c84e617921c2d61a158ab712d
Yongguang Qin <yongguang.qin@gmail.com>
2021-01-13 22:32:10 -0500

    Removed some code

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,6 @@
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://toutiao.com")
 print(r.status_code)
+print(r.ok)
